e6e1b23ea430f3f6a5f9a9b72c4c7fee2519b71b
gregpaton08 <gregpaton08@gmail.com>
2015-11-25 19:02:38 -0500

    [mod] Added login screen.

---
+++ Light Switch/LoginViewController.h
@@ -0,0 +1,17 @@
+//
+//  LoginViewController.h
+//  Light Switch
+//
+//  Created by Greg Paton on 11/25/15.
+//  Copyright © 2015 Greg Paton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+@property (strong, nonatomic) IBOutlet UITextField *tfUsername;
+@property (strong, nonatomic) IBOutlet UITextField *tfPassword;
+
+- (IBAction)login:(id)sender;
+
+@end
---
+++ Light Switch/LoginViewController.m
@@ -0,0 +1,40 @@
+//
+//  LoginViewController.m
+//  Light Switch
+//
+//  Created by Greg Paton on 11/25/15.
+//  Copyright © 2015 Greg Paton. All rights reserved.
+//
+
+#import "LoginViewController.h"
+
+@interface LoginViewController ()
+
+@end
+
+@implementation LoginViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+- (IBAction)login:(id)sender {
+    [self performSegueWithIdentifier:@"loginSuccess" sender:self];
+}
+@end
