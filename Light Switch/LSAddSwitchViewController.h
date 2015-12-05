a6cf227f9ed5d3534127f2dbe6a971eeebca451f
gregpaton08 <gregpaton08@gmail.com>
2015-12-05 15:56:33 -0500

    [add] Added view for adding a switch.

---
+++ Light Switch/LSAddSwitchViewController.h
@@ -0,0 +1,13 @@
+//
+//  LSAddSwitchViewController.h
+//  Light Switch
+//
+//  Created by Greg Paton on 12/5/15.
+//  Copyright © 2015 Greg Paton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LSAddSwitchViewController : UIViewController
+
+@end
---
+++ Light Switch/LSAddSwitchViewController.m
@@ -0,0 +1,37 @@
+//
+//  LSAddSwitchViewController.m
+//  Light Switch
+//
+//  Created by Greg Paton on 12/5/15.
+//  Copyright © 2015 Greg Paton. All rights reserved.
+//
+
+#import "LSAddSwitchViewController.h"
+
+@interface LSAddSwitchViewController ()
+
+@end
+
+@implementation LSAddSwitchViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
